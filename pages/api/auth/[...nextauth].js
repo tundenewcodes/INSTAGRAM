8a85e7f7fa6092001a106f020577346e125d77bf
tunde <fadipe11@gmail.com>
2022-07-23 02:26:58 +0100

    secret key

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,7 +9,7 @@ export default NextAuth({
         }),
         // ...add more providers here
     ],
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: 'glory',
     pages: {
         signIn: '/auth/signin'
     },
